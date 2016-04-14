617351b2ed0c03a1ecb7a7d5533533ce46892397
ktwingstrom <ktwingstrom@gmail.com>
2016-04-13 19:00:16 -0600

    changed to gmail mailto handler

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -4,6 +4,7 @@ function sendMessage()
   var email = document.getElementById("userEmail").value;
   var message = document.getElementById("userMessage").value;
 
-  window.location.href = 'mailto:ktwingstrom@gmail.com?subject=RE:%20Resume%20Website&body='+name+'%0A'+email+'%0A%0A'+message+'';
+  //window.location.href = 'mailto:ktwingstrom@gmail.com?subject=RE:%20Resume%20Website&body='+name+'%0A'+email+'%0A%0A'+message+'';
+  window.location.href='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=target@email.com';
 
 }
