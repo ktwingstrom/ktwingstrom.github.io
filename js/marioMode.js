63a72e4a68b9f3a3cc9aa4f254a960eb8b1c17f8
ktwingstrom <ktwingstrom@gmail.com>
2016-04-17 16:32:32 -0600

    removed video files and music, added embedded player

--- js/marioMode.js
+++ js/marioMode.js
@@ -2,7 +2,7 @@ var secret = "7765827379"; //mario
 var input = "";
 var timer;
 var mode = false;
-var marioMusic = document.getElementById("musicPlayer");
+
 
 $(document).keyup(function(e) {
     //alert(e.which);
@@ -18,9 +18,9 @@ function check_input() {
     if(input == secret) {
         //the secret code
         alert("Welcome to Mario Mode!!");
-        $('#kevinPic').attr("src", 'img/marioHead.png').css("padding",'130px 130px');
-        $('.site').css('background-image','url(img/mario.png)');
-        musicPlayer.play();
+        $('#kevinPic').hide();
+        $('.mainContainer').prepend('<div align="center"><object width="640" height="480"><param name="movie" value="http://www.embeddablegames.net/embedgames/superflashmariobros.swf"><param name="quality" value="high"><embed src="http://www.embeddablegames.net/embedgames/superflashmariobros.swf" quality="high" width="640" height="400" type="application/x-shockwave-flash" pluginspage= "http://www.macromedia.com/go/getflashplayer"></embed></object><br/></div><p>');
+        $('#site').css('background-image', 'url(img/mario.png)');
 
 
     }
